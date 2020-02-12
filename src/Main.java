c22f29f05cdcfbefa430b9afcee4aec52560b078
Leonard Dizon <leonard.dizon@codeandtheory.com>
2020-02-12 14:27:26 +0800

    Inital commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import com.leondzn.library.MusicRoom;
+
+public class Main {
+  public static void main(String[] args) {
+    MusicRoom musicRoom = new MusicRoom();
+
+    //musicRoom.addInstrument(your, instruments, here);
+
+    musicRoom.display();
+  }
+}
